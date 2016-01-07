9f43e94c7d7559e185f6f037d135be02cfd66562
Chris Leishman <chris@leishman.org>
2016-01-07 10:22:11 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Neo4j Homebrew Tap
+------------------
+
+This repository is a Tap for the [Homebrew package manager](http://brew.sh/) for Mac OS X.
+
+Install packages from this repository as follows:
+
+```console
+$ brew install cleishm/neo4j/libneo4j-client
+```
+
+You can also install the bleeding edge development versions using:
+
+```console
+$ brew install --HEAD cleishm/neo4j/libneo4j-client
+```
+
+License
+-------
+
+libneo4j-client is licensed under the [Apache License, Version 2.0](
+http://www.apache.org/licenses/LICENSE-2.0).
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations under the License.
---
+++ libneo4j-client.rb
@@ -0,0 +1,48 @@
+class Libneo4jClient < Formula
+  desc "Shell client and C driver for Neo4j"
+  homepage "https://cleishm.github.com/libneo4j-client"
+  url "https://github.com/cleishm/libneo4j-client/releases/download/v0.3.0/libneo4j-client-0.3.0.tar.gz"
+  sha256 "1d65e3b4b6ad587ef56e85bcddd8fa2246ab8374eab08b2def98810d3d0a0cd4"
+
+  head do
+    url "https://github.com/cleishm/libneo4j-client.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+    depends_on "peg" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "LDFLAGS=-L#{Formula["openssl"].opt_prefix}",
+                          "CPPFLAGS=-I#{Formula["openssl"].opt_prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+        #include <neo4j-client.h>
+        #include <stdio.h>
+
+        int main(int argc, char *argv[])
+        {
+            neo4j_client_init();
+            neo4j_value_t v = neo4j_string("hello world");
+            char buf[128];
+            printf(\"%s\\n\", neo4j_tostring(v, buf, sizeof(buf)));
+            neo4j_client_cleanup();
+            return 0;
+        }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lneo4j-client", "-o", "test"
+    system "./test | grep 'hello world'"
+  end
+end
