fdd7d7f256f5c2ce61d2d3059b6f3498c1ac24a1
Chris Leishman <chris@leishman.org>
2016-05-23 05:41:30 -0400

    Bump version

--- Formula/libcypher-parser.rb
+++ Formula/libcypher-parser.rb
@@ -1,8 +1,8 @@
 class LibcypherParser < Formula
   desc "C parsing library and command line linter for the Cypher Query Language"
   homepage "https://cleishm.github.io/libcypher-parser/"
-  url "https://github.com/cleishm/libcypher-parser/releases/download/v0.1.0/libcypher-parser-0.1.0.tar.gz"
-  sha256 "4ad857c5ae869491d276448a382bbdf70eee23cd3e634a500c13a5b3ffb77e84"
+  url "https://github.com/cleishm/libcypher-parser/releases/download/v0.2.0/libcypher-parser-0.2.0.tar.gz"
+  sha256 "0f45273f2808f2a73c9e7cf7a9ba138ba839244b276e7fb4ebb948e3bbadffd0"
 
   head do
     url "https://github.com/cleishm/libcypher-parser.git"
